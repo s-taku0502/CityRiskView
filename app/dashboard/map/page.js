976d3dc47d0a937e7516bf4bf98b0a0acae681db
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-04-30 00:02:55 +0900

    fix:地図の二重表示を解除

--- app/dashboard/map/page.js
+++ app/dashboard/map/page.js
@@ -1,9 +1,10 @@
 // 地図だけ表示する画面
 "use client"; // ← これを追加
 
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
+import Map from "@/components/Map";
 
-const Map = dynamic(() => import("@/components/Map"), { ssr: false });
+// const Map = dynamic(() => import("@/components/Map"), { ssr: false });
 
 export default function MapPage() {
   return (
