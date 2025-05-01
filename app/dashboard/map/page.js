590c3f73d3197d4ac5e0be22093eb93b99d23366
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-05-01 11:44:20 +0900

    del:不要なコメントアウトを削除

--- app/dashboard/map/page.js
+++ app/dashboard/map/page.js
@@ -1,10 +1,6 @@
 // 地図だけ表示する画面
 "use client";
-
-// import dynamic from "next/dynamic";
 import Map from "@/components/Map";
-
-// const Map = dynamic(() => import("@/components/Map"), { ssr: false });
 
 export default function MapPage() {
   return (
