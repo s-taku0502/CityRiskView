61f56328fb02d95eba8121fdd894871bb42960cb
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-05-03 09:27:52 +0900

    add:pageの追加

--- app/dashboard/map/page.js
+++ app/dashboard/map/page.js
@@ -1,13 +1,15 @@
 // 地図だけ表示する画面
 "use client";
-import Map from "@/components/Map";
+import Map from "@/app/dashboard/map/components/Map";
+import DisasterMap from "@/app/dashboard/map/components/DisasterMap";
 
 export default function MapPage() {
   return (
     <div className="space-y-4">
       <h2 className="text-xl font-bold">🗺️ 現在地マップ</h2>
       <div className="bg-white rounded-lg shadow">
-        <Map />
+        {/* <Map /> */}
+        <DisasterMap />
       </div>
     </div>
   );
