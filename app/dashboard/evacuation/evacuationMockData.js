91cacf4ac84e770115af96ff63c0a16a570eefc8
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-05-22 04:16:16 +0900

    避難場所の情報を仮決め

---
+++ app/dashboard/evacuation/evacuationMockData.js
@@ -0,0 +1,32 @@
+export const mockShelters = [
+  {
+    id : "tomi-taka001",
+    name : "高岡市役所",
+    status : "開設中",
+    capacity : 150,
+    current_people : 15,
+    stock_alert : false,
+    orders : ["避難指示（高齢者）", "レベル3"],
+    last_updated : "2025-05-22 00:00",
+  },
+  {
+    id : "tomi-tomi000",
+    name : "富山県庁",
+    status : "開設中",
+    capacity : 300,
+    current_people : 15,
+    stock_alert : false,
+    orders : ["避難指示（高齢者）", "レベル3"],
+    last_updated : "2025-05-22 00:00",
+  },
+  {
+    id : "tomi-tomi001",
+    name : "富山市役所",
+    status : "開設中",
+    capacity : 300,
+    current_people : 15,
+    stock_alert : false,
+    orders : ["避難指示（高齢者）", "レベル3"],
+    last_updated : "2025-05-22 00:00",
+  }
+]
