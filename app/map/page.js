26510c0daa8bee4a1f347c2db081205f1d422adc
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-06-30 13:51:22 +0900

    サンプル画面（地図）の公開

--- app/map/page.js
+++ app/map/page.js
@@ -34,8 +34,8 @@ export default function MapPage() {
     <div className="space-y-4">
       <h2 className="text-xl font-bold">現在地マップ</h2>
       <div className="rounded-lg shadow">
-        <p>ただいま地図情報においてメンテナンスをおこなっています。</p>
-        {/* <Map /> */}
+        {/* <p>ただいま地図情報においてメンテナンスをおこなっています。</p> */}
+        <Map />
       </div>
     </div>
   );
