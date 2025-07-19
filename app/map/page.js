ad8b6a2f7e49b1f780b426a9277a936aecd993c5
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-07-19 14:52:59 +0900

    現在地再取得ボタンの位置を修正するため、このページから削除

--- app/map/page.js
+++ app/map/page.js
@@ -37,19 +37,7 @@ export default function MapPage() {
         {/* <p>ただいま地図情報においてメンテナンスをおこなっています。</p> */}
         <Map />
         {/*MapBox 著作権のカスタム表記 */}
-        <div className="absolute bottom-2 text-xs text-gray-500 bg-white bg-opacity-80 px-2 py-1 rounded">
-          © <a href="https://www.mapbox.com/about/maps/" target="_blank" rel="noopener noreferrer" className="underline">Mapbox</a> |
-          © <a href="https://www.openstreetmap.org/about/" target="_blank" rel="noopener noreferrer" className="underline">OpenStreetMap</a>
-        </div>
       </div>
-      {/* ページが正しく表示されない場合は、
-      <button
-        onClick={() => window.location.reload()}
-        className="mt-2 text-blue-600 font-bold py-2 px-4 rounded"
-      >
-        こちら
-      </button>
-      をクリックして再読み込みしてください。 */}
     </div>
   );
 }
