8de0760581ea9170c8fcfcfde1ce27eb434d86ab
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-08-18 17:30:31 +0900

    Add Google Analytics tracking

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-Y37BP6LQGG');
+            gtag('config', 'G-xxxxxxxx');
           `}
         </Script>
         
